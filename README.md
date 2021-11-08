f11229e828875f15993960d797cad0beb60eff79
JunHee Lee <jhlee910609@gmail.com>
2021-11-08 22:58:38 +0900

    feat: add examples

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # practicing-redux-toolkit-with-vanilla-js
+
+> redux를 vanilla-js에 생으로 구현해보자!
+
+- [Simple Redux Introduction in Vanilla Javascript - Understand Redux in 20 Minutes - YouTube](https://www.youtube.com/watch?v=hMSTO4cpPaQ&ab_channel=MonsterlessonsAcademy)]
---
+++ src/main.js
@@ -0,0 +1,55 @@
+/**
+ * Redux
+ * single flow of data is a main benefit of Redux!
+ */
+
+/**
+ * reducer is a function which get specific data back.
+ * @param {*} state: global object with data.
+ * @param {string} action: action.
+ */
+const reducer = (state = [], action) => {
+  // logic about state.
+  // state is current state.
+  switch (action.type) {
+    case "ADD_USER":
+      return [...state, action.payload];
+  }
+  return state;
+};
+
+/**
+ * store
+ * store is instance of Redux.
+ */
+const store = Redux.createStore(reducer);
+
+const list = document.querySelector(".list");
+const btnAddUser = document.querySelector(".addUser");
+const userInput = document.querySelector(".userInput");
+
+btnAddUser.addEventListener("click", () => {
+  store.dispatch({ type: "ADD_USER", payload: userInput.value });
+});
+
+/**
+ * module 1
+ * store.getState(): return current state.
+ */
+store.subscribe(() => {
+  userInput.value = "";
+  const frag = document.createDocumentFragment();
+  store.getState().forEach((user) => {
+    const li = document.createElement("li");
+    li.innerText = user;
+    frag.appendChild(li);
+  });
+  list.replaceChildren(frag);
+});
+
+/**
+ * module 2
+ * The object inside dispatch is an action.
+ * @param {string} type: type is a manadatory and is always string.
+ * @param {*} payload: payload is a property to store data in action.
+ */
